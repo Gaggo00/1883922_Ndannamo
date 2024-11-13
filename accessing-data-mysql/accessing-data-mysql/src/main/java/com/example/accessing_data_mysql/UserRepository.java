e6b7996926effe0e469a680c5442b1881e15587d
Gaggo00 <gavrieldnp@gmail.com>
2024-11-13 20:25:05 +0100

    collegamento mysql a java

---
+++ accessing-data-mysql/accessing-data-mysql/src/main/java/com/example/accessing_data_mysql/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.accessing_data_mysql;
+
+import org.springframework.data.repository.CrudRepository;
+import com.example.accessing_data_mysql.User;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+}
