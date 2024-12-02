88af87bd80c863d668ff88b8f5a298500146ed04
jfabi88 <jacopo.fabi8@gmail.com>
2024-12-02 16:03:35 +0100

    Quasi completata pagina per selezionare i trips

--- ndannamo_project/frontend/src/components/TripButton/TripButton.js
+++ ndannamo_project/frontend/src/components/TripButton/TripButton.js
@@ -6,12 +6,12 @@ function TripButton({destination, data, bg_color}) {
     console.log(bg_color)
     return (
         <div className='mainContainer' style={{backgroundColor: bg_color}}>
-            <div className='secondaryContainer' style={{flex: '4'}}>
+            <div className='secondaryContainer'>
                 <div className='destination'>{destination}</div>
                 <div className='data'>{data}</div>
             </div>
-            <div style={{height: '100%', alignContent: 'center', flex: '1'}}>
-                <BsChevronRight style={{height: '30%', width: '100%'}}/>
+            <div style={{alignContent: 'center', flex: '0.3'}}>
+                <BsChevronRight/>
             </div>
         </div>
     );
--- ndannamo_project/frontend/src/pages/Main.js
+++ ndannamo_project/frontend/src/pages/Main.js
@@ -1,13 +1,52 @@
 import React from 'react';
 import TripButton from '../components/TripButton/TripButton'
+import "../styles/Main.css"
 
 function Main() {
 
+    const colors = [
+        '#FFBC70',
+        '#FFF4C7',
+    ]
+
+    var upcoming_list = [
+        {destination: 'Parigi', data: '22/03 - 26/03'},
+        {destination: 'Londra', data: '28/09 - 2/10'},
+        {destination: 'Milano', data: '28/09 - 2/10'}
+    ]
+
+    var past_list = [
+        {destination: 'Parigi', data: '22/03 - 26/03'},
+        {destination: 'Londra', data: '28/09 - 2/10'},
+        {destination: 'Milano', data: '28/09 - 2/10'}
+    ]
 
     return (
-        <div style={{ textAlign: 'center', marginTop: '50px' }}>
-            <TripButton destination="Sasso" data="22/03 - 26/03" bg_color='#FFBC70'/>
-            <h1>Questo é il main</h1>
+        <div className='page-container'>
+            <div className='section-container'>
+                <div className='title-text'>Upcoming trips</div>
+                <div className='trips-container'>
+                    {upcoming_list.map((trip, index) => (
+                        <TripButton
+                            destination={trip.destination}
+                            data={trip.data}
+                            bg_color={colors[index % colors.length]}
+                        />
+                    ))}
+                </div>
+            </div>
+            <div className='section-container'>
+                <div className='title-text'>Past trips</div>
+                <div className='trips-container'>
+                    {past_list.map((trip, index) => (
+                        <TripButton
+                            destination={trip.destination}
+                            data={trip.data}
+                            bg_color={'#E9E9E9'}
+                        />
+                    ))}
+                </div>
+            </div>
         </div>
     );
 }
