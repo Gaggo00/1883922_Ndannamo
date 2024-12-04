92125ffcc618648b4e7e3642015e5e36a6c25dd8
jfabi <jacopo.fabi8@gmail.com>
2024-12-04 15:07:38 +0100

    Rimozione .idea

--- ndannamo_project/frontend/src/pages/Main.js
+++ ndannamo_project/frontend/src/pages/Main.js
@@ -6,7 +6,15 @@ import Trip from '../models/Trip';
 function Main() {
 
     var trips = [
-        new Trip("Milano", 1725456488000, 1725629288000)
+        new Trip("Parigi", 1739194088000, 1739294088000),
+        new Trip("Londra", 1736456488000, 1737629288000),
+        new Trip("Roma", 1756456488000, 1762629288000),
+        new Trip("Amsterdam", 1773456488000, 1778629288000),
+        new Trip("Praga", 1725456488000, 1725629288000),
+        new Trip("Stoccolma", 1725456488000, 1725629288000),
+        new Trip("Praga", 1725456488000, 1725629288000),
+        new Trip("Milano", 1725456488000, 1725629288000),
+        new Trip("Napoli", 1725456488000, 1725629288000),
     ]
 
     var upcoming_list = [
